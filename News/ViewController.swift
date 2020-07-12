c8bb29ee58192eec12618d1041a4addc930d597e
Angelina Shamanova <angelinashamanova@gmail.com>
2020-07-12 13:28:15 +0300

    Initial Commit

---
+++ News/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by MacBook on 12/07/2020.
+//  Copyright © 2020 mybestcompany.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
